962f71af0687a58da00d60731e5965d39dac5484
toolesstes <toolsgest@gmail.com>
2026-04-24 20:18:40 +0530

    update

--- app/services/cache.py
+++ app/services/cache.py
@@ -1,17 +1,49 @@
-import os
+# import os
 
+# import redis
+# import json
+
+# REDIS_URL = os.getenv("REDIS_URL")
+# r = redis.Redis(host="localhost", port=6379, decode_responses=True)
+
+
+# def get_cache(key):
+#     data = r.get(key)
+#     return json.loads(data) if data else None
+
+
+# def set_cache(key, value, expire=3600):  # 👈 ADD expire
+#     r.set(key, value, ex=expire)
+
+import os
 import redis
 import json
 
 REDIS_URL = os.getenv("REDIS_URL")
-r = redis.Redis(host="localhost", port=6379, decode_responses=True)
+
+# ✅ Use Render Redis URL (IMPORTANT)
+try:
+    r = redis.from_url(REDIS_URL, decode_responses=True)
+except:
+    r = None
 
 
 def get_cache(key):
-    data = r.get(key)
-    return json.loads(data) if data else None
+    try:
+        if r:
+            data = r.get(key)
+            return json.loads(data) if data else None
+    except:
+        return None
 
 
-def set_cache(key, value, expire=3600):  # 👈 ADD expire
-    r.set(key, value, ex=expire)
+def set_cache(key, value, expire=3600):
+    try:
+        if r:
+            # always store as string
+            if isinstance(value, dict):
+                value = json.dumps(value)
 
+            r.set(key, value, ex=expire)
+    except:
+        pass
