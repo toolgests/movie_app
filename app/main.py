c1c571b1e50b45d654c4980b82c7e14e28c13a67
toolesstes <toolsgest@gmail.com>
2026-04-25 18:27:21 +0530

    fix cors properly

--- app/main.py
+++ app/main.py
@@ -1,8 +1,17 @@
 from app.services.tmdb import start_scheduler
 from fastapi import FastAPI
 from app.api.routes import movies, details
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],      # allow all origins (for now)
+    allow_credentials=True,
+    allow_methods=["*"],      # GET, POST, PUT, DELETE etc.
+    allow_headers=["*"],
+)
 
 app.include_router(movies.router)
 app.include_router(details.router)
